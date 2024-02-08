463fcb2aa16456ce1f76876f0fa2063ab3fa65a6
Antony Vasiliev <tony.vasiliev8@gmail.com>
2024-02-07 21:26:41 -0500

    Documented API a bit

--- web_api.js
+++ web_api.js
@@ -9,24 +9,40 @@ class WebAPI{
         this._app = null;
         this._server = null;
         this._router = null;
+
+        this._isRunning = false;
     }
 
+    // Log a request
+    // Takes a request object
+    // Resolves if successful
+    // Rejects if not
     _log(req){
         return new Promise((resolve, reject) => {
+            // Simply log some stuff
             console.log(`${new Date().toLocaleString()} - ${req.socket.address().address} | ${req.method} ${req.url}`);
 
             resolve();
         })
     }
 
+    // Generate a JSON error response object
+    // Takes a status code, message and details strings
+    // Returns a JSON formatted object string
     _generateErrorResponseBody(status, message, details){
         return JSON.stringify({ status, message, details });
     }
 
-    _generatePlugResponseBody(data){
+    // Generate a JSON response object
+    // Takes a block of data
+    // Returns a JSON string containing the data and a 200 status code
+    _generateResponseBody(data){
         return JSON.stringify({ status: 200, data });
     }
 
+    // Get plugs from database
+    // Takes nothing
+    // Resolves with the plug rows
     _getPlugs(){
         return new Promise((resolve, reject) => {
             // Query database
@@ -39,7 +55,10 @@ class WebAPI{
         })
     }
 
-    _getPlugResults(plugID, start, end){
+    // Get results from database
+    // Takes a plug ID, and optionally start and end timestamps
+    // Resolves the results for plug between the 2 timestamps
+    _getPlugResults(plugID, start = null, end = null){
         return new Promise((resolve, reject) => {
             // Query database
             this._database.getPlugResults(plugID, start, end).then(results => {
@@ -51,9 +70,13 @@ class WebAPI{
         })
     }
 
+    // Setup API routes
+    // Takes nothing
+    // Resolves if setup successful
     _setupRouter(){
         let router = express.Router();
 
+        // GET all plug results
         router.get('/plug_results', (req, res) => {
             // Set headers
             res.setHeader('Content-Type', 'application/json');
@@ -61,47 +84,59 @@ class WebAPI{
             // Get optional start and end timestamps
             const {start, end} = req.query;
 
+            // Query for plugs
             this._getPlugs()
             .catch(err => {
+                // Failed to get plugs
                 return Promise.reject(`Failed to get plugs: ${err.message}`);
             })
             .then(plugs => {
                 let plugResults = {};
                 let promises = [];
 
+                // Iterate over every plug in database
                 for (let plug of plugs){
                     let id = plug['plug_id'];
                     let name = plug['plug_name'];
 
+                    // Query results for plug between the timestamps
                     let prom = this._getPlugResults(id, start, end)
                     .then(results => {
+                        // Save results when done
                         plugResults[name] = results
                     })
 
                     promises.push(prom);
                 }
 
+                // Wait for all queries to finish
                 return Promise.all(promises)
                 .then(() => {
                     return plugResults;
                 })
                 .catch(err => {
+                    // Failed to get results
                     return Promise.reject(`Failed to get plug results: ${err.message}`);
                 })
             .then(results => {
-                res.send(this._generatePlugResponseBody({ results }))
+                // Generate and send response
+                res.send(this._generateResponseBody({ results }))
             })
             .catch(err => {
                 console.error(`Error: Failed to responsd with all plugs results: ${err.message}`);
+
+                // Send server error response
                 res.statusCode = 500;
                 res.send(this._generateErrorResponseBody(500, 'Server error', `Failed to get plug results`))
             })
             })
             .finally(() => {
+                // End response
                 res.end();
             })
         })
 
+        // Get results for a particular plug
         router.get('/plug_results/:id', (req, res) => {
             // Set headers
             res.setHeader('Content-Type', 'application/json');
@@ -114,9 +149,12 @@ class WebAPI{
             // Query database for the results
             this._getPlugResults(id, start, end)
             .then(results => {
-                res.send(this._generatePlugResponseBody({ results }))
+                // Generate and send response
+                res.send(this._generateResponseBody({ results }))
             }).catch(err => {
                 console.error(`Error: Error: Failed to respond with results for plug ${id}: ${err.message}`);
+
+                // Send server error response
                 res.statusCode = 500;
                 res.send(this._generateErrorResponseBody(500, 'Server error', `Failed to get plug results`))
             })
@@ -125,7 +163,9 @@ class WebAPI{
             })
         })
 
+        // If using endpoint other than the ones above
         router.use((req, res) => {
+            // Send not found response
             res.statusCode = 404;
             res.end(this._generateErrorResponseBody(404, 'Resource not found', `API endpoint "${req.url}" does not exist`));
         })
@@ -133,48 +173,76 @@ class WebAPI{
         return router;
     }
 
+    // Start the API server
+    // Takes nothing
+    // Resolves when done
     start(){
         return new Promise((resolve, reject) => {
-            // Setup Express
-            this._app = express();
-            this._server = http.createServer(this._app);
-            this._router = this._setupRouter();
+            // Make sure not already running
+            if (!this._isRunning){
+                this._isRunning = true;
 
-            // Attach logger
-            this._app.use('/', (req, res, next) => {
-                this._log(req)
-                .then(() => {
-                    // Continue with the request
-                    next();
+                // Setup Express
+                this._app = express();
+                this._server = http.createServer(this._app);
+                this._router = this._setupRouter();
+
+                // Attach logger
+                this._app.use('/', (req, res, next) => {
+                    this._log(req)
+                    .then(() => {
+                        // Continue with the request
+                        next();
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.end('Failed to log!')
+                    })
                 })
-                .catch(err => {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.end('Failed to log!')
+
+                // Attach API route
+                this._app.use('/api/', this._router);
+
+                // Attach 404
+                this._app.use((req, res) => {
+                    res.status = 404;
+                    res.end(this._generateErrorResponseBody(404, 'Resource not found',`"${req.url}" was not found`));
                 })
-            })
 
-            // this._app.use(express.json());
+                // Start server
+                this._server.listen(this._port, () => {
+                    resolve();
+                })
 
-            // Attach API route
-            this._app.use('/api/', this._router);
-
-            // Attach 404
-            this._app.use((req, res) => {
-                res.status = 404;
-                res.end(this._generateErrorResponseBody(404, 'Resource not found',`"${req.url}" was not found`));
-            })
-
-            // Start server
-            this._server.listen(this._port, () => {
-                resolve();
-            })
+                // If server crashes
+                this._server.on('error', (err) => {
+                    reject(new Error(`Failed to start server: ${err.message}`))
+                })
+            }
+            else{
+                reject(new Error(`Server already running!`))
+            }
         })
     }
 
+    // Stop the API server
+    // Takes nothing
+    // Resolves when done
     stop(){
         return new Promise((resolve, reject) => {
-            resolve();
+            // If server is running
+            if (this._server){
+                // Close server
+                this._server.close(err => {
+                    if (err){
+                        reject(new Error(`Failed to close server: ${err.message}`))
+                    }
+                    else{
+                        resolve();
+                    }
+                })
+            }
         })
     }
 }
